44ea7feed711e44699f265f00171bbb68e530976
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-06-19 17:49:45 -0400

    set same-site to none

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ app.use(
       client: clientP,
     }),
     cookie: {
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
