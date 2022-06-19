a1ea133220772a405223f213bcf3497a0389a21c
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2022-06-19 18:15:56 -0400

    updated google oauth

--- server/config/passport.js
+++ server/config/passport.js
@@ -39,7 +39,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL:
+        (process.env.NODE_ENV === "production" &&
+          "https://the-study-node.herokuapp.com") + "/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       User.findOne({ googleId: profile.id }, async (err, user) => {
