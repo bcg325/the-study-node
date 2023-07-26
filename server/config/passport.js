503074c6b2896acdc543b97c6682bacbfad5df0f
Bryan Chavez-Gonzalez <60202482+bcg325@users.noreply.github.com>
2023-07-25 19:56:02 -0700

    updated passport google redirect

--- server/config/passport.js
+++ server/config/passport.js
@@ -39,7 +39,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://studynode.up.railway.app/auth/google/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       const googleEmail = profile.emails[0].value;
